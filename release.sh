6478f2eb3254678dba8ffd0b7548ff7a286e50e1
kakie <782922086@qq.com>
2022-06-22 09:23:44 +0800

    build & release

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## Auto Build & Tag & Release
+## Auto Tag & Release
 
 if [ $# -ne 1 ]
 then
@@ -9,23 +9,6 @@ then
 fi
 
 version=$1
-arch=amd64
 chksum=wordle-bundle_${version}_checksums.txt
 
-buildBinary () {
-	local name=$1
-	for sys in "darwin" "linux" "windows"; do 
-		printf "sys: %s arch: %s" $sys $arch
-		echo ""
-		GOOS=$sys GOARCH=amd64 go build -o $name cmd/${name}/main.go
-		tar czvf ${name}_${version}_${sys}_${arch}.tar.gz $name
-		rm $name
-	done
-}
-
-for name in "wordle-puzzle"; do
-	buildBinary $name
-	shasum -a 256 ${name}_${version}_*.tar.gz >> ${chksum}
-done
-
-gh release create ${version} -t ${version} -F - ./${name}_${version}_*.tar.gz ./${chksum}
+gh release create ${version} -t ${version} -F - ./wordle-*_${version}_*.tar.gz ./${chksum}
